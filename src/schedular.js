04e07cdfc12c3a0804132e1cf21e7266fc0483f7
othree_kao <othree_kao@htc.com>
2013-09-04 17:28:48 +0800

    Basic Schedular codes, add row/col

---
+++ src/schedular.js
@@ -0,0 +1,90 @@
+/*jslint vars: true */
+/*global $: false, Schedular: false, Templates: false */
+
+(function (root) {
+    "use strict";
+
+    var i;
+
+    var defaults = function (options, defaults) {
+        var opt;
+        options = options || {};
+        for (opt in defaults) {
+            if (options[opt] === undefined) {
+                options[opt] = defaults[opt];
+            }
+        }
+        return options;
+    };
+
+    var Obj = function () {
+        return {};
+    };
+
+    var Arr = function (len) {
+        var array = [];
+        for (i = 0; i < len; i++) {
+            array.push(new Obj());
+        }
+        return array;
+    };
+
+    root.Schedular = function ($el, options) {
+        this.$el = $el;
+        this.init(defaults(options, {
+            col: 2,
+            row: 6
+        }));
+    };
+
+    Schedular.prototype = {
+        init: function (options) {
+            this.$el.append(Templates.base());
+            this.$root = this.$el.find('.root');
+            this.$colRoot = this.$el.find('.col-root');
+            this.options = options;
+            this.grid = [];
+            for (i = 0; i < this.options.row; i++) {
+                this.addRow();
+            }
+            for (i = 0; i < this.options.col; i++) {
+                this.addCol();
+            }
+        },
+
+        addRow: function () {
+            this.row++;
+            this.grid.push(new Arr(this.col));
+            this.addRowNode();
+        },
+        addRowNode: function () {
+            var $row = $(Templates.row());
+            for (i = 0; i < this.col; i++) {
+                $row.append(Templates.cell());
+            }
+            this.$root.append($row);
+            this.$colRoot.find('.col').each(function () {
+                $(this).append(Templates.colcell());
+            });
+        },
+
+        addCol: function () {
+            this.col++;
+            for (i = 0; i < this.row; i++) {
+                this.grid[i].push(new Obj());
+            }
+            this.addColNode();
+        },
+        addColNode: function () {
+            var $col = $(Templates.col());
+            for (i = 0; i < this.row; i++) {
+                $col.append(Templates.colcell());
+            }
+            this.$colRoot.append($col);
+            this.$root.find('.row').each(function () {
+                $(this).append(Templates.cell());
+            });
+        }
+    };
+
+}(this));
