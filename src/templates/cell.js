e9ae104ea4154410219e8f868cab82d3318c767b
othree_kao <othree_kao@htc.com>
2013-09-26 16:19:59 +0800

    Add cell content and cell mover

--- src/templates/cell.js
+++ src/templates/cell.js
@@ -1,3 +1,3 @@
 this["Templates"] = this["Templates"] || {};
 
-this["Templates"]["cell"] = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");_.b("<div class=\"cell\"></div>");_.b("\n");return _.fl();;});
+this["Templates"]["cell"] = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");_.b("<div class=\"cell\">");_.b("\n" + i);_.b("    <div class=\"cell-mover\"></div>");_.b("\n" + i);_.b("    <div class=\"cell-content\"></div>");_.b("\n" + i);_.b("</div>");_.b("\n");return _.fl();;});
